b2a8f4cf4c8ff56e3ea3358224a02d7725416839
Ricardo Cesar Rodrigues <62864640+rickkk856@users.noreply.github.com>
2021-04-14 01:24:02 -0300

    Add files via upload

--- tidydf.py
+++ tidydf.py
@@ -19,13 +19,13 @@ from extrafuns import *
 
 
 def gettidydf():
-    config_file = open('./config.txt', 'r')
+    config_file = open('./config.txt', 'r', encoding='utf-8')
     yyi = config_file.readlines()[5].split(':')[1]
     yyi = yyi.rstrip('\n')
     yyi = yyi.strip(' ')
     yyi = float(yyi)
     config_file.close()
-    config_file = open('./config.txt', 'r')
+    config_file = open('./config.txt', 'r', encoding='utf-8')
     yyf = config_file.readlines()[6].split(':')[1]
     yyf = yyf.rstrip('\n')
     yyf = yyf.strip(' ')
