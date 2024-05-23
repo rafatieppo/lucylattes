979ba6cd7ee214eb997288f24c49e61d006a8321
rafatieppo <rafaeltieppo@yahoo.com.br>
2024-05-23 18:26:03 -0400

    clean files for a new version

--- readidlist.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-
-def readIdList():
-    df_idnamelist = pd.read_csv('list_id_name.txt', dtype='str',
-                                skiprows=4, header=0,
-                                sep=',')
-    return(df_idnamelist)
-# ------------------------------------------------------------
