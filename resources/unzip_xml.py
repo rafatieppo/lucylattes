b931959620e6383d2b88b802a46e2621cb82336e
rafatieppo <rafaeltieppo@yahoo.com.br>
2024-10-03 13:36:39 -0400

    it was add an exception to read the xml file from zip file. It was because the name of the xml file from export system is different of the site. Notice that the xml is not available on the cv lattes site.

--- resources/unzip_xml.py
+++ resources/unzip_xml.py
@@ -8,6 +8,10 @@ def unzip_xml(zipname):
     # zipname = '3275865819287843.zip'
     zipfilepath = './xml_zip' + '/' + str(zipname)
     archive = zipfile.ZipFile(zipfilepath, 'r')
-    xmlFile = archive.open('curriculo.xml')
-    print('A xml file from ', zipname, ' has been created.')
+    try:
+        xmlFile = archive.open('curriculo.xml')
+        print('A xml file from ', zipname, ' has been created.')
+    except KeyError:
+        xmlFile = archive.open(zipname.split('.')[0] + '.xml')
+        print('A xml file from ', zipname, ' has been created.')
     return xmlFile
